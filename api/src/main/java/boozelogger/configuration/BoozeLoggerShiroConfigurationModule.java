c617dccc4e2d68e1f7be339e2d3f285ef52b529c
Cory Johannsen <cory.johannsen@vendscreen.com>
2014-05-07 11:31:57 -0700

    Remove .iml files Added .iml to .gitignore Refactored BoozeLoggerShiroConfigurationModule for default basic auth

--- api/src/main/java/boozelogger/configuration/BoozeLoggerShiroConfigurationModule.java
+++ api/src/main/java/boozelogger/configuration/BoozeLoggerShiroConfigurationModule.java
@@ -26,7 +26,7 @@ public class BoozeLoggerShiroConfigurationModule extends LDAPShiroConfigurationM
     @SuppressWarnings("unchecked")
     @Override
     protected void configureFilterChains() {
-        Key<SharedSecretAuthenticatingFilter> SHARED_SECRET = Key.get(SharedSecretAuthenticatingFilter.class);
+        //Key<SharedSecretAuthenticatingFilter> SHARED_SECRET = Key.get(SharedSecretAuthenticatingFilter.class);
 
         // API Version
         String apiVersion = System.getProperty(GrandUnificationModule.API_VERSION);
@@ -35,7 +35,11 @@ public class BoozeLoggerShiroConfigurationModule extends LDAPShiroConfigurationM
         // Filters are evaluated in the order in which they are defined so the least specific should be added as the last filter
         // chain. E.g. /** will match any url so  it should be the last added to the the filters collection.
 
-        addFilterChain("/" + apiVersion + "/*", NO_SESSION_CREATION, SHARED_SECRET);
+        // The Shared Secret filter is custom to the unification api. It will return either a UsernamePasswordToken if
+        // the client  is a generic caller, or a custom SharedSecretToken if it detects the presence of a custom http
+        // header.  Since it handles the generation of both authentication tokens will use, we only need the one
+        // filter to perform authentication.
+        addFilterChain("/" + apiVersion + "/*", NO_SESSION_CREATION, AUTHC_BASIC);
         addFilterChain("/**", ANON);
     }
 }
